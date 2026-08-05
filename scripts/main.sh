13faf8c62cc8bbe651f51499686312ac713dc8ba
Minerte <wphetchun@gamil.com>
2026-08-05 23:11:15 +0200

    aaaaa

--- scripts/main.sh
+++ scripts/main.sh
@@ -78,8 +78,8 @@ function main_install_gentoo_in_chroot() {
     echo "Uncommented and set CPU_FLAGS_X86 in make.conf"
     sleep 5
 
-    # einfo "Re-emerge ALL system apps"
-    # try emerge --emptytree -1 @installed
+    einfo "Re-emerge ALL system apps"
+    try emerge --emptytree -1 @installed
 
     echo "merging filesystem"
     try emerge --verbose sys-fs/cryptsetup sys-fs/btrfs-progs \
@@ -92,13 +92,13 @@ function main_install_gentoo_in_chroot() {
 
     install_kernel
 
-    # echo "Emerging tools"
-    # try emerge --verbose sys-block/io-scheduler-udev-rules \
-    #     sys-apps/mlocate dev-vcs/git net-misc/networkmanager \
-    #     app-shells/bash-completion net-misc/chrony app-admin/sysklogd \
-    #     sys-process/cronie sys-auth/seatd
+    echo "Emerging tools"
+    try emerge --verbose sys-block/io-scheduler-udev-rules \
+        sys-apps/mlocate dev-vcs/git net-misc/networkmanager \
+        app-shells/bash-completion net-misc/chrony app-admin/sysklogd \
+        sys-process/cronie sys-auth/seatd
 
-    # enable_service
+    enable_service
 
     echo "Set root password"
     passwd
@@ -124,7 +124,6 @@ function install_kernel() {
     cd /usr/src/linux \
         || die "could not change to /usr/linux"
 
-    sleep 5
     zcat /proc/config.gz > .config
     make olddefconfig || die "make olddefconfig failed"
     echo "olddefconfig dubug message only"
@@ -133,7 +132,6 @@ function install_kernel() {
     kernel_script
 
     sleep 5
-    einfo "Update kernel"
     make olddefconfig || die "make olddefconfig failed after scripts/config"
     sleep 5
 
