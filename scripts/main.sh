b1040f15c910b8da08446132711131143c50215a
Minerte <wphetchun@gamil.com>
2026-08-12 18:17:48 +0200

    comment out recomplie to test the new useflags

--- scripts/main.sh
+++ scripts/main.sh
@@ -94,18 +94,18 @@ function main_install_gentoo_in_chroot() {
 
     install_kernel
 
-    echo "Emerging tools"
-    try emerge --verbose sys-block/io-scheduler-udev-rules \
-        sys-apps/mlocate dev-vcs/git net-misc/networkmanager \
-        app-shells/bash-completion net-misc/chrony app-admin/sysklogd \
-        sys-process/cronie sys-auth/seatd
+    # echo "Emerging tools"
+    # try emerge --verbose sys-block/io-scheduler-udev-rules \
+    #     sys-apps/mlocate dev-vcs/git net-misc/networkmanager \
+    #     app-shells/bash-completion net-misc/chrony app-admin/sysklogd \
+    #     sys-process/cronie sys-auth/seatd
 
-    enable_service
+    # enable_service
 
     echo "Set root password"
     passwd
 
-    try emerge --verbose x11-drivers/xf86-video-nouveau media-libs/mesa
+    # try emerge --verbose x11-drivers/xf86-video-nouveau media-libs/mesa
 
     einfo "script completed"
 }
