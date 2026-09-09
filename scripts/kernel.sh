973b8f3cd9f2cd1a2a7d9e5fcf056aa523fd7664
Minerte <wphetchun@gmail.com>
2026-09-09 20:59:36 +0200

    more

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -49,7 +49,7 @@ function kernel_script() {
 	local swap_partuuid="${CHROOT_SWAP_PARTUUID:-}"
 
 	local cmdline
-	cmdline="root=PARTUUID=${root_partuuid} rootfstype=btrfs resume=PARTUUID=${swap_partuuid} initrd=\\EFI\\BOOT\\ugrd.cpio rw quiet loglevel=3"
+	cmdline="root=PARTUUID=${root_partuuid} rootfstype=btrfs resume=PARTUUID=${swap_partuuid} initrd=\\\\EFI\\\\BOOT\\\\ugrd.cpio rw quiet loglevel=3"
 
 
     # Enable the command line with PARTUUID
